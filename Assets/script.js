108798d8a4bca96c037f1672ede5cfcf6cf8cabd
Dylan Hagens <dylan.hagens@gmail.com>
2023-04-13 12:22:07 -0500

    added variables in Java

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,38 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var startDate = new Date("2023-05-01");
+var currentDate = dayjs(currentDate);
+
+$("#currentDay").text(currentDate.format("MM DD, YYYY"));
+
+var hour;
+
+var currentTime = function () {
+  var date = new Date ();
+  var hh = date.getHours();
+  var mm = date.getMinutes();
+  var ss = date.getSeconds();
+  var session = "AM";
+  hour = hh;
+  if(hh > 12) {
+    hh = hh - 12;
+    session = "PM"
+  }
+
+  hh = (hh < 10) ? "0" + hh : hh;
+  mm = (mm < 10) ? "0" + mm : mm;
+  ss = (ss < 10) ? "0" + ss : ss;
+
+  var time = hh+ ":" + mm + ":" + ss + "" + session;
+
+  document.getElementById("currentTime").innerText = time;
+  var t = setTimeout(function(){ currentTime()}, 1000);
+}
+currentTime();
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
