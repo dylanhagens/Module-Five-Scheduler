f768fa21bf9f6743b26c8c6ae768464853b954c5
Dylan Hagens <dylan.hagens@gmail.com>
2023-04-13 12:33:30 -0500

    added function to javascript

--- Assets/script.js
+++ Assets/script.js
@@ -28,28 +28,33 @@ var currentTime = function () {
   var time = hh+ ":" + mm + ":" + ss + "" + session;
 
   document.getElementById("currentTime").innerText = time;
-  var t = setTimeout(function(){ currentTime()}, 1000);
+  var time = setTimeout(function(){ currentTime()}, 1000);
 }
 currentTime();
 
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+  var saveEl = $(".saveBtn");
+  
+    saveEl.on("click", function(event) {
+      event.preventDefault();
+  
+      var text = $(this).prev().val()
+  
+      localStorage.setItem($(this).parent().attr("id"),text);
+    })
+    
+    for(i =0;i < 24; i++) {
+      $(`#hour-${i + 1}`).children("textarea").val(localStorage.getItem(`hour-${i + 1}`))
+  
+      if(hour === i) {
+        $(`#hour-${i + 1}`).children("textarea").addClass("present")
+      } else if (hour > i) {
+        $(`#hour-${i + 1}`).children("textarea").addClass("past")
+      } else {
+        $(`#hour-${i + 1}`).children("textarea").addClass("future")
+      }
+    }
+  
+  });
